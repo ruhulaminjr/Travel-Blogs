9a5eca2f3a1fd45023ecc5e4f153aa81491103ac
Ruhul Amin <ruhulaminjr@debian-BULLSEYE-live-builder-AMD64>
2022-01-27 23:55:48 +0600

    bug fixed

--- src/Pages/HomePage/Blogs/Blogs.js
+++ src/Pages/HomePage/Blogs/Blogs.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import React, { useEffect } from "react";
-import { useState } from "react/cjs/react.development";
+import React, { useEffect, useState } from "react";
+
 import Blog from "./Blog";
 
 const Blogs = () => {
