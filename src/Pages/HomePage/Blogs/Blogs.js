916d552bb7e98b2455aa9b6e8520e66fa689e54e
Ruhul Amin <ruhulaminjr@debian-BULLSEYE-live-builder-AMD64>
2022-01-28 22:05:06 +0600

    pagination done

--- src/Pages/HomePage/Blogs/Blogs.js
+++ src/Pages/HomePage/Blogs/Blogs.js
@@ -6,15 +6,19 @@ import Blog from "./Blog";
 const Blogs = () => {
   const [blogsData, setBlogsData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
   useEffect(() => {
     setLoading(true);
     axios
-      .get("https://rocky-inlet-47708.herokuapp.com/gethomeblogs")
+      .get(`http://localhost:5000/gethomeblogs?page=${currentPage}`)
       .then((result) => {
-        setBlogsData(result.data);
+        setBlogsData(result.data.allblogs);
         setLoading(false);
+        const pageNumber = Math.ceil(result.data.blogsCount / 10);
+        setPageCount(pageNumber);
       });
-  }, []);
+  }, [currentPage]);
   if (loading) {
     return (
       <div className="container mx-auto h-full w-full flex items-center justify-center">
@@ -28,6 +32,25 @@ const Blogs = () => {
         {blogsData.map((blogItem) => (
           <Blog key={blogItem._id} blog={blogItem} />
         ))}
+      </div>
+      <div className="my-2 flex items-center justify-center">
+        <ul className="flex pl-0 list-none rounded my-2">
+          {[...Array(pageCount).keys()].map((number) => (
+            <li
+              className={`relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 border-r-0 hover:bg-gray-200 ${
+                currentPage === number ? "bg-gray-300 shadow font-bold" : ""
+              }`}
+              key={number}
+            >
+              <button
+                className="page-link"
+                onClick={() => setCurrentPage(number)}
+              >
+                {number}
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
